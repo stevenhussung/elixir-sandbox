2727cc6335239153d974e8dfba0d5e902a655983
Steve Hussung <shussung@indot.in.gov>
2024-03-12 13:27:15 -0400

    Read in file, hello world in project

--- copycat/lib/copycat.ex
+++ copycat/lib/copycat.ex
@@ -11,8 +11,14 @@ defmodule Copycat do
       iex> Copycat.hello()
       :world
 
+  ## Start info:
+
+  To load into this, use "iex -S mix"
   """
   def hello do
     :world
+    IO.puts("Hello in there, hello")
+
+    File.read("flightsToLAX.json")
   end
 end
--- hello_world.exs
+++ hello_world.exs
@@ -1,5 +1,9 @@
 IO.puts("Hello world")
 
 # Next task: read the json.
-# Subtask: install the package.
-# Subsubtask get mix working again.
+# Subtask: install the package. Check!
+# Subsubtask get mix working again. Check!
+
+#Subtask: Hello world from the project Check!
+#Subtask: Read the json file in as text. Check!)
+#Subtask: Parse! That! Json!
